6f8b66e415a3c093ad82fde61c5456b22accffae
Ridgley, Randy <22918922+randyridgley@users.noreply.github.com>
2021-04-14 12:28:08 -0400

    fix IAM role for lakeformation service principal and remove crawler code

--- resources/code/infra/lib/glue-studio-snowflake-stack.ts
+++ resources/code/infra/lib/glue-studio-snowflake-stack.ts
@@ -4,7 +4,6 @@ import * as s3 from '@aws-cdk/aws-s3';
 import * as s3deploy from '@aws-cdk/aws-s3-deployment';
 import * as iam from '@aws-cdk/aws-iam';
 import * as secrets from '@aws-cdk/aws-secretsmanager';
-import * as databrew from '@aws-cdk/aws-databrew';
 import * as lf from '@aws-cdk/aws-lakeformation';
 import * as path from 'path';
 
@@ -20,7 +19,10 @@ export class GlueStudioSnowflakeStack extends cdk.Stack {
     super(scope, id, props);
 
     const glueRole = new iam.Role(this, 'GlueExecutionRole', {
-      assumedBy: new iam.ServicePrincipal('glue.amazonaws.com'),
+      assumedBy: new iam.CompositePrincipal(
+        new iam.ServicePrincipal('glue.amazonaws.com'), 
+        new iam.ServicePrincipal('lakeformation.amazonaws.com')
+      ),
       managedPolicies: [
         iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSGlueServiceRole")
       ]
@@ -142,48 +144,5 @@ export class GlueStudioSnowflakeStack extends cdk.Stack {
         'DESCRIBE'
       ]
     });
-
-    const crawler = new glue.CfnCrawler(this, 'nfl-data-crawler', {
-      role: glueRole.roleName,
-      targets: {
-        s3Targets: [{
-          path: `s3://${workingBucket.bucketName}/football/nfl/`,
-          exclusions: ['boxScoreByPlayer/**']
-        }]
-      },
-      databaseName: nflDatabase.databaseName,
-      description: 'Crawler to populate the nfl database with data from Amazon S3',
-      name: 'nfl-data-crawler'      
-    });
-    // const databrewRecipe = new databrew.CfnRecipe(this, 'clean-stats-recipe', {
-    //   name: 'clean-nfl-stats-recipe',
-    //   steps: [{
-    //     action: {
-    //       operation: "CHANGE_DATA_TYPE",
-    //       parameters: {
-    //         "columnDataType": "integer",
-    //         "sourceColumn": "team_id"
-    //       }
-    //     }
-    //   },
-    //   {
-    //     action: {
-    //       operation: "CHANGE_DATA_TYPE",
-    //       parameters: {            
-    //         "columnDataType": "integer",
-    //         "sourceColumn": "player_id"
-    //       }
-    //     }
-    //   },
-    //   {
-    //     action: {
-    //       operation: "DELETE",          
-    //       parameters: {
-    //         "sourceColumns": "[\"blocked_fgs\",\"blocked_kicks\",\"blocked_pat\",\"blocked_punts\",\"def_fums\",\"def_fums_lost\",\"fg_atts\",\"fg_convs\",\"fg_convs_details\",\"fg_long\",\"fg_missed_details\",\"fgs_blocked\",\"kick_ret_atts\",\"kick_ret_long\",\"kick_ret_long_is_td\",\"kick_ret_tds\",\"kick_ret_yds\",\"kick_rets_over_40_yds\",\"kick_rets_over_40_yds_for_td\",\"misc_tkls\",\"net_yds_per_punt\",\"pat_atts\",\"pat_blocked\",\"pat_convs\",\"punt_long\",\"punt_ret_atts\",\"punt_ret_fair_catches\",\"punt_ret_long\",\"punt_ret_long_is_td\",\"punt_ret_tds\",\"punt_rets_over_40_yds\",\"punt_rets_over_40_yds_for_td\",\"punt_return_yds\",\"punt_returns\",\"punt_touch_backs\",\"punt_yds\",\"punts\",\"punts_blocked\",\"punts_in_20\",\"special_teams_fums\",\"special_teams_fums_lost\",\"special_teams_tkl_asts\",\"special_teams_tkls\",\"misc_tkls_asts\"]"
-    //       }
-    //     }
-    //   }],
-    //   description: 'Recipe to clean stats and remove columns not needed'
-    // })
   }
 }
